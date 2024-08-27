599a2913cd35ddbcff858f7711d5132f09b786d9
rafagomez92 <gomez92rafael@gmail.com>
2024-08-27 00:16:06 -0600

    feat: adding chat and splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/screens/auth.dart';
+import 'package:chat_app/screens/chat.dart';
+import 'package:chat_app/screens/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -20,6 +23,16 @@ class App extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(
               seedColor: const Color.fromARGB(255, 63, 17, 177)),
         ),
-        home: const AuthScreen());
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (ctx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+              if (snapshot.hasData) {
+                return const ChatScreen();
+              }
+              return const AuthScreen();
+            }));
   }
 }
